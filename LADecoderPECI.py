c389e775831e577a4d3fe41d5e0c99eba1e307dd
Lin <ian.lin@intel.com>
2024-11-20 08:34:02 +0800

    Title: Added WrEndPointConfig(C5) parsing flow and data decoder while read/write command, Descript:     1. Add new feild "R/W Data" feild decoded the data while send        WrEndPointConfig/RdEndPointConfig PECI command     2. Add WrEndPointConfig(C5) PCI/MMIO command decoder support

--- LADecoderPECI.py
+++ LADecoderPECI.py
@@ -94,13 +94,14 @@ def main():
             # initial feild
             #       inser new feild
             #       Node(CPU) / BDF / reg offset / PECI Completion code /
-            new_field_names = ['PECI Rerty', 'CPU', 'Bus', 'Device', 'Function', 'Register/MMIO Offset', 'PECI Completion Code', ' '] + original_field_names
+            new_field_names = ['PECI Rerty', 'CPU', 'Bus', 'Device', 'Function', 'Register/MMIO Offset', 'R/W Data', 'PECI Completion Code', ' '] + original_field_names
             #       fill data in new insered feild
             dict_row_LA_RAW['CPU'] = 'N/A'
             dict_row_LA_RAW['Bus'] = 'N/A'
             dict_row_LA_RAW['Device'] = 'N/A'
             dict_row_LA_RAW['Function'] = 'N/A'
             dict_row_LA_RAW['Register/MMIO Offset'] = 'N/A'
+            dict_row_LA_RAW['R/W Data'] = 'N/A' 
             dict_row_LA_RAW['PECI Completion Code'] = 'N/A'
 
             #
@@ -146,7 +147,12 @@ def main():
                 # RdEndPointConfig() Local PCI Cfg or PCI Cfg - Message Type = 3,4
                 DecodeRdEndPointConfig_PciCfg (dict_row_LA_RAW)
                 # RdEndPointConfig() MMIO - Message Type = 5
-                DecodeRdEndPointConfig_Mmio (dict_row_LA_RAW)         
+                DecodeRdEndPointConfig_Mmio (dict_row_LA_RAW)
+            if (dict_row_LA_RAW['Command(h)'] == 'WrEndPointConfig(C5)'):
+                # WrEndPointConfig() Local PCI Cfg or PCI cfg - Message Type = 3,4
+                DecodeWrEndPointConfig_PciCfg (dict_row_LA_RAW)
+                # WrEndPointConfig() MMIO - Message Type = 5
+                DecodeWrEndPointConfig_Mmio  (dict_row_LA_RAW)
             
             #Completion Code - Byte0 in 'Read Data' column
             data = dict_row_LA_RAW['Read Data(h)']
@@ -187,35 +193,66 @@ def main():
 def DecodeRdEndPointConfig_PciCfg (row_LA_RAW):
     if (row_LA_RAW['Command(h)'] == 'RdEndPointConfig(C1)'):
         RawWriteData = row_LA_RAW['Write Data(h)'].split()
-        hex_value = int(RawWriteData[0], 16)
-        HostID = (hex_value >> 1) & 0x7F
-        Retry = hex_value & 0x01
-        # PCIe Cfg Address
-        hex_value = 0
-        selected_data = RawWriteData[7:11]
-        if (selected_data):
-            reversed_list = selected_data[::-1]
-            hex_value_str = ''.join(reversed_list)
-            hex_value = int(hex_value_str, 16)
-        # Fetch BDF
         MessageType = int(RawWriteData[1], 16)
-        if (MessageType == 0x03):               # Local PCI-Cfg
-            Bus = '{} (Local PCI-Cfg)'.format((hex_value >> 20) & 0xFFF)
-        elif (MessageType == 0x04):               # PCI-Cfg
-            Bus = '{} (PCI-Cfg)'.format((hex_value >> 20) & 0xFFF)
-        else:
-            Bus = (hex_value >> 20) & 0xFFF
-        Device = (hex_value >> 15) & 0x1F
-        Function = (hex_value >> 12) & 0x03
-        RegOffset = (hex_value) & 0x0FFF
-                        
-        row_LA_RAW['PECI Rerty'] = Retry
-        row_LA_RAW['Bus'] = Bus
-        row_LA_RAW['Device'] = Device
-        row_LA_RAW['Function'] = Function
-        row_LA_RAW['Register/MMIO Offset'] = hex(RegOffset)
-        debug_print ("debug:")
-        debug_print (row_LA_RAW)
+        debug_print ('debug: RdEndPointConfig Message Type = {}'.format(MessageType))
+        if (MessageType == 0x03 or MessageType == 0x04):                           # Message Type (3-Local PCI Cfg, 4-PCI)
+            debug_print ('debug: RawWriteData = {}'.format(RawWriteData))
+            hex_value = int(RawWriteData[0], 16)
+            HostID = (hex_value >> 1) & 0x7F
+            Retry = hex_value & 0x01
+            # PCIe Cfg Address
+            hex_value = 0
+            selected_data = RawWriteData[7:11]
+            if (selected_data):
+                reversed_list = selected_data[::-1]
+                hex_value_str = ''.join(reversed_list)
+                hex_value = int(hex_value_str, 16)
+            # Fetch BDF
+            MessageType = int(RawWriteData[1], 16)
+            if (MessageType == 0x03):               # Local PCI-Cfg
+                Bus = '{} (Local PCI-Cfg)'.format((hex_value >> 20) & 0xFFF)
+            elif (MessageType == 0x04):               # PCI-Cfg
+                Bus = '{} (PCI-Cfg)'.format((hex_value >> 20) & 0xFFF)
+            else:
+                # Doc only define Message Type = 3, 4
+                return
+            Device = (hex_value >> 15) & 0x1F
+            Function = (hex_value >> 12) & 0x03
+            RegOffset = (hex_value) & 0x0FFF
+
+            # Read Data
+            if (row_LA_RAW['Read Data(h)'] is not None and row_LA_RAW['Read Data(h)'].strip() != ''):
+                RawReadData = row_LA_RAW['Read Data(h)'].split()
+                ReadData = 0
+                hex_value = 0
+                StringBytes = '(x Byte)'
+                PeciRow_RdLen = row_LA_RAW['RdLen']   # use RdLen to identify recieved data length
+                if (PeciRow_RdLen == '2'):     # 1 byte
+                    selected_data = RawReadData[1]
+                    hex_value_str = f"0x{selected_data:02X}"
+                    StringBytes = '(1 Byte)'
+                elif (PeciRow_RdLen == '3'):   # 2 bytes
+                    selected_data = RawReadData[-2:]
+                    reversed_list = selected_data[::-1]
+                    hex_value = int(reversed_list, 16)
+                    hex_value_str = f"0x{hex_value:04X}"
+                    StringBytes = '(2 Bytes)'
+                elif (PeciRow_RdLen == '5'):   # 4 bytes
+                    selected_data = RawReadData[-4:]
+                    reversed_list = selected_data[::-1]
+                    hex_value = int((''.join(reversed_list)), 16)
+                    hex_value_str = f"0x{hex_value:08X}"
+                    StringBytes = '(4 Bytes)'
+
+                row_LA_RAW['R/W Data'] = hex_value_str + StringBytes
+                debug_print ('debug: store row_LA_RAW[RW Data] = {}'.format(row_LA_RAW['R/W Data']))
+                            
+            row_LA_RAW['PECI Rerty'] = Retry
+            row_LA_RAW['Bus'] = Bus
+            row_LA_RAW['Device'] = Device
+            row_LA_RAW['Function'] = Function
+            row_LA_RAW['Register/MMIO Offset'] = f"0x{RegOffset:04X}"
+            debug_print ('debug: {}'.format(row_LA_RAW))
 
 #
 # 5.8.11.2 Command Format for RdEndPointConfig() MMIO
@@ -256,23 +293,254 @@ def DecodeRdEndPointConfig_Mmio (row_LA_RAW):
                 reversed_list = selected_data[::-1]
                 hex_value_str = ''.join(reversed_list)
                 hex_value = int(hex_value_str, 16)
-            RegOffsetString = "BAR:{} / Offset(32bits):{}".format(int(RawWriteData[4], 16), hex(hex_value))
+            RegOffsetString = "BAR:{} / Offset(32bits):0x{:04X}".format(int(RawWriteData[4], 16), hex_value)
         elif (AddressType == 6): # 64bits Addressing
             selected_data = RawWriteData[9:17]
             if (selected_data):
                 reversed_list = selected_data[::-1]
                 hex_value_str = ''.join(reversed_list)
                 hex_value = int(hex_value_str, 16)
-            RegOffsetString = "BAR:{} / Offset(64bits):{}".format(int(RawWriteData[4], 16), hex(hex_value))
+            RegOffsetString = "BAR:{} / Offset(64bits):0x{:04X}".format(int(RawWriteData[4], 16), hex_value)
         else:
             RegOffsetString = 'Address Type Error'
         debug_print ("debug: {}".format(RegOffsetString))
-                        
+        # Read Data
+        if (row_LA_RAW['Read Data(h)'] is not None and row_LA_RAW['Read Data(h)'].strip() != ''):
+            RawReadData = row_LA_RAW['Read Data(h)'].split()
+            ReadData = 0
+            hex_value = 0
+            StringBytes = '(x Byte)'
+            PeciRow_RdLen = row_LA_RAW['RdLen']   # use RdLen to identify recieved data length
+            if (PeciRow_RdLen == '2'):     # 1 byte
+                selected_data = RawReadData[1]
+                hex_value_str = f"0x{selected_data:02X}"
+                StringBytes = '(1 Byte)'
+            elif (PeciRow_RdLen == '3'):   # 2 bytes
+                selected_data = RawReadData[-2:]
+                reversed_list = selected_data[::-1]
+                hex_value = int(reversed_list, 16)
+                hex_value_str = f"0x{hex_value:04X}"
+                StringBytes = '(2 Bytes)'
+            elif (PeciRow_RdLen == '5'):   # 4 bytes
+                selected_data = RawReadData[-4:]
+                reversed_list = selected_data[::-1]
+                hex_value = int((''.join(reversed_list)), 16)
+                hex_value_str = f"0x{hex_value:08X}"
+                StringBytes = '(4 Bytes)'
+            elif (PeciRow_RdLen == '9'):   # 8 bytes
+                selected_data = RawReadData[-8:]
+                reversed_list = selected_data[::-1]
+                hex_value = int((''.join(reversed_list)), 16)
+                hex_value_str = f"0x{hex_value:016X}"
+                StringBytes = '(8 Bytes)'
+                
+            row_LA_RAW['R/W Data'] = hex_value_str + StringBytes
+            debug_print ('debug: store row_LA_RAW[RW Data] = {}'.format(row_LA_RAW['R/W Data']))
+
+
         row_LA_RAW['PECI Rerty'] = Retry
-        row_LA_RAW['Bus'] = '{} (0x{})'.format(Bus, hex(Bus))
+        row_LA_RAW['Bus'] = '{} (MMIO)'.format(Bus)
         row_LA_RAW['Device'] = Device
         row_LA_RAW['Function'] = Function
         row_LA_RAW['Register/MMIO Offset'] = RegOffsetString
+
+#
+# 5.8.12.1 Command Format for WrEndPointConfig() Local PCI Cfg or PCI Cfg
+#
+# Figure 5-25 (data feild from byte 4)
+#   Byte 4(0):                  HostID[7:1] / Retry [0]
+#   Byte 5(1):                  Message Type (3-Local PCI Cfg, 4-PCI)
+#   Byte 6(2):                  EndPointID
+#   Byte 7/8(3,4):              Reserved
+#   Byte 9(5):                  Address Type (0x04)
+#   Byte 10(6):                 PCIe Segment
+#   Byte 11-14(7,8,9,10):       PCIe Cfg Address 
+#                                   Bit[31:20]: Bus
+#                                   Bit[19:15]: Device
+#                                   Bit[14:12]: Function
+#                                   Bit[11:0]: Register Offset
+#                                       ex. "008181A0" -> Bus:8 / Dev:3 / Function:0 / Reg: 0x1A0
+#   Byte 15-18(11,12,13,14):    Data (1, 2, 4 bytess)
+#                                   LSB -> MSB
+#
+def DecodeWrEndPointConfig_PciCfg (row_LA_RAW):
+    if (row_LA_RAW['Command(h)'] == 'WrEndPointConfig(C5)'):
+        RawWriteData = row_LA_RAW['Write Data(h)'].split()
+        debug_print ("debug: original Write Data feild - {}".format(RawWriteData))
+        MessageType = int(RawWriteData[1], 16)
+        if (MessageType == 0x03 or MessageType == 0x04):                           # Message Type (3-Local PCI Cfg, 4-PCI)
+            hex_value = int(RawWriteData[0], 16)
+            HostID = (hex_value >> 1) & 0x7F
+            Retry = hex_value & 0x01
+            # PCIe Cfg Address
+            hex_value = 0
+            selected_data = RawWriteData[7:11]
+            if (selected_data):
+                reversed_list = selected_data[::-1]
+                hex_value_str = ''.join(reversed_list)
+                hex_value = int(hex_value_str, 16)
+            # Fetch BDF
+            MessageType = int(RawWriteData[1], 16)
+            if (MessageType == 0x03):               # Local PCI-Cfg
+                Bus = '{} (Local PCI-Cfg)'.format((hex_value >> 20) & 0xFFF)
+            elif (MessageType == 0x04):               # PCI-Cfg
+                Bus = '{} (PCI-Cfg)'.format((hex_value >> 20) & 0xFFF)
+            Device = (hex_value >> 15) & 0x1F
+            Function = (hex_value >> 12) & 0x03
+            RegOffset = (hex_value) & 0x0FFF
+            # Data be writen
+            hex_value = 0
+            StringBytes = '(x Byte)'
+            PeciRow_WrLen = row_LA_RAW['WrLen'].split()   # use WRLen to identify wroten data length
+            if (PeciRow_WrLen == '14'):     # 1 byte
+                selected_data = RawWriteData[11]
+                hex_value_str = f"0x{selected_data:02X}"
+                StringBytes = '(1 Byte)'
+            elif (PeciRow_WrLen == '15'):   # 2 bytes
+                selected_data = RawWriteData[11:13]
+                reversed_list = selected_data[::-1]
+                hex_value = int(reversed_list, 16)
+                hex_value_str = f"0x{hex_value:04X}"
+                StringBytes = '(2 Bytes)'
+            elif (PeciRow_WrLen == '17'):   # 4 bytes
+                selected_data = RawWriteData[11:15]
+                reversed_list = selected_data[::-1]
+                hex_value = int((''.join(reversed_list)), 16)
+                hex_value_str = f"0x{hex_value:08X}"
+                StringBytes = '(4 Bytes)'
+
+            if (selected_data):
+                row_LA_RAW['R/W Data'] = hex_value_str + StringBytes
+                debug_print ('debug: store row_LA_RAW[RW Data] = {}'.format(row_LA_RAW['R/W Data']))
+
+            row_LA_RAW['PECI Rerty'] = Retry
+            row_LA_RAW['Bus'] = Bus
+            row_LA_RAW['Device'] = Device
+            row_LA_RAW['Function'] = Function
+            row_LA_RAW['Register/MMIO Offset'] = f"0x{RegOffset:04X}"
+            debug_print ("debug:")
+            debug_print (row_LA_RAW)
+
+#
+# 5.8.12.2 Command Format for WrEndPointConfig() MMIO
+#
+# Figure 5-26 (data feild from byte 4)
+#   Byte 4(0):              HostID[7:1] / Retry [0]
+#   Byte 5(1):              Message Type (5)
+#   Byte 6(2):              EndPointID
+#   Byte 7(3):              Reserved
+#   Byte 8(4):              BAR(Memory type)
+#   Byte 9(5):              Address Type (0x05:32bit addressing, 0x06: 64bits addressing)
+#   Byte 10(6):             PCIe Segment
+#   Byte 11(7):             Device/Func
+#                               Bit[7:3]: Device
+#                               Bit[2:0]: Function
+#   Byte 12(8):             Bus
+#   Byte 13-16(9-12):       Address (32Bits addressing)
+#   or 
+#   Byte 13-20(9-16):       Address (64Bits addressing)
+#   
+#   Byte 21-28(17-24):      Data (1, 2, 4, 8 bytess)
+#
+def DecodeWrEndPointConfig_Mmio (row_LA_RAW):
+    if (row_LA_RAW['Command(h)'] == 'WrEndPointConfig(C5)'):
+        RawWriteData = row_LA_RAW['Write Data(h)'].split()
+        debug_print ("debug: DecodeWrEndPointConfig_Mmio() original Write Data feild - {}".format(RawWriteData))
+        MessageType = int(RawWriteData[1], 16)
+        if (MessageType == 0x05):                           # MMIO
+            hex_value = int(RawWriteData[0], 16)
+            HostID = (hex_value >> 1) & 0x7F
+            Retry = hex_value & 0x01
+            BAR = int(RawWriteData[4], 16)
+            Bus = int(RawWriteData[8], 16)
+            Device = (int(RawWriteData[7], 16) >> 3) & 0x01F
+            Function = int(RawWriteData[7], 16) & 0x03
+            hex_value = 0
+            # MMIO Offset (Address feild iin spec)
+            AddressType = int(RawWriteData[5], 16)
+            debug_print ("debug: DecodeWrEndPointConfig_Mmio()  {}".format(RawWriteData))
+            if (AddressType == 5): # 32bits Addressing
+                selected_data = RawWriteData[9:13]
+                if (selected_data):
+                    reversed_list = selected_data[::-1]
+                    hex_value_str = ''.join(reversed_list)
+                    hex_value = int(hex_value_str, 16)
+                RegOffsetString = "BAR:{} / Offset(32bits):0x{:04X}".format(int(RawWriteData[4], 16), hex_value)
+            elif (AddressType == 6): # 64bits Addressing
+                selected_data = RawWriteData[9:17]
+                if (selected_data):
+                    reversed_list = selected_data[::-1]
+                    hex_value_str = ''.join(reversed_list)
+                    hex_value = int(hex_value_str, 16)
+                RegOffsetString = "BAR:{} / Offset(64bits):0x{:04X}".format(int(RawWriteData[4], 16), hex_value)
+            else:
+                RegOffsetString = 'Address Type Error'
+            debug_print ("debug: {}".format(RegOffsetString))
+            # Data be writen
+            selected_data = 0
+            hex_value = 0
+            StringBytes = '(x Byte)'
+            PeciRow_WrLen = row_LA_RAW['WrLen']   # use WRLen to identify wroten data length
+            # WrLen = 16 (Byte), 17 (Word), 19 (DWord), 23 (QWord) for Address Type 0x05
+            if (AddressType == 5):           
+                if (PeciRow_WrLen == '16'):     # 1 byte
+                    selected_data = RawWriteData[17]
+                    hex_value_str = f"0x{selected_data:02X}"
+                    StringBytes = '(1 Byte)'
+                elif (PeciRow_WrLen == '17'):   # 2 bytes
+                    selected_data = RawWriteData[17:19]
+                    reversed_list = selected_data[::-1]
+                    hex_value = int(reversed_list, 16)
+                    hex_value_str = f"0x{hex_value:04X}"
+                    StringBytes = '(2 Bytes)'
+                elif (PeciRow_WrLen == '19'):   # 4 bytes
+                    selected_data = RawWriteData[17:21]
+                    reversed_list = selected_data[::-1]
+                    debug_print (reversed_list)
+                    
+                    hex_value = int(''.join(reversed_list), 16)
+                    hex_value_str = f"0x{hex_value:08X}"
+                    StringBytes = '(4 Bytes)'
+                elif (PeciRow_WrLen == '23'):   # 8 bytes
+                    selected_data = RawWriteData[17:25]
+                    reversed_list = selected_data[::-1]
+                    hex_value = int(reversed_list, 16)
+                    hex_value_str = f"0x{hex_value:016X}"
+                    StringBytes = '(8 Bytes)'
+
+            # WrLen = 20 (Byte), 21 (Word), 23 (DWord), 27 (QWord) for Address Type 0x06
+            elif (AddressType == 6):           
+                if (PeciRow_WrLen == '20'):     # 1 byte
+                    selected_data = RawWriteData[17]
+                    hex_value_str = f"0x{selected_data:02X}"
+                    StringBytes = '(1 Byte)'
+                elif (PeciRow_WrLen == '21'):   # 2 bytes
+                    selected_data = RawWriteData[17:19]
+                    reversed_list = selected_data[::-1]
+                    hex_value = int(reversed_list, 16)
+                    hex_value_str = f"0x{hex_value:04X}"
+                    StringBytes = '(2 Bytes)'
+                elif (PeciRow_WrLen == '23'):   # 4 bytes
+                    selected_data = RawWriteData[17:21]
+                    reversed_list = selected_data[::-1]
+                    hex_value = int(reversed_list, 16)
+                    hex_value_str = f"0x{hex_value:08X}"
+                    StringBytes = '(4 Bytes)'
+                elif (PeciRow_WrLen == '27'):   # 8 bytes
+                    selected_data = RawWriteData[17:25]
+                    reversed_list = selected_data[::-1]
+                    hex_value = int(reversed_list, 16)
+                    hex_value_str = f"0x{hex_value:016X}"
+                    StringBytes = '(8 Bytes)'
+
+            row_LA_RAW['R/W Data'] = hex_value_str + StringBytes
+            debug_print ('debug: store row_LA_RAW[RW Data] = {}'.format(row_LA_RAW['R/W Data']))
+
+            row_LA_RAW['PECI Rerty'] = Retry
+            row_LA_RAW['Bus'] = '{} (MMIO)'.format(Bus)
+            row_LA_RAW['Device'] = Device
+            row_LA_RAW['Function'] = Function
+            row_LA_RAW['Register/MMIO Offset'] = RegOffsetString
 
 def debug_print(*args, sep=' ', end='\n'):
     global DebugFlag
